1d5a4e57ca54a92bd6da4852b86bdd338c93e9be
felipeViana <felipe.140492@gmail.com>
2016-05-21 22:37:10 -0300

    cria api pra pegar o tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :tests
   root 'principal#index'
+  get '/test_set/:video_url', to: 'api#find_test_set_from_video'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
